2de657b5ed609f80655e129008ffb7053f2e49ad
juliovbo-Brocom <juliovbo@gmail.com>
2025-10-02 12:28:55 -0300

    Add files via upload

---
+++ app-estetica-vehicular-pwa/sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'ev-cache-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest'
+];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', (e) => {
+  const { request } = e;
+  e.respondWith(
+    caches.match(request).then(cached => cached || fetch(request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE).then(c => c.put(request, copy));
+      return resp;
+    }).catch(() => caches.match('/index.html')))
+  );
+});
